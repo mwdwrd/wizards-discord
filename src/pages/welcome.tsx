3a9aeaed61ef54be0ddf9ec08db3048d7dd3955b
Matty Woodward <mwdwrd@gmail.com>
2021-09-21 22:09:50 -0400

    Roles Update

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -4,7 +4,11 @@ const WelcomePage = () => {
   return (
     <>
       <h1>The Wizards</h1>
-      <div className="message">Welcome! You have been added to the Discord.</div>
+      <div className="message">
+        Welcome! You have been added to the Wizards Build.
+        <br />
+        Visit Discord in your <a href="https://discord.com/">browser</a> or app.
+      </div>
     </>
   );
 };
